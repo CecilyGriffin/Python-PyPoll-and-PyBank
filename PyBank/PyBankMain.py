dd4fa89a0a7301f3961075f138e2405c70e3fce9
Mercy Griffin <teachmercy@gmail.com>
2020-05-08 20:45:17 -0500

    add ProfitLoss and month count

--- PyBank/PyBankMain.py
+++ PyBank/PyBankMain.py
@@ -8,10 +8,18 @@ csvpath = os.path.join(".", "PyBankResources", "budget_data.csv")
 #Read using CSV module
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-# Read the header row first
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    for row in csvreader:
-        print(row[0])
-        #print(row)
+    ProfitLoss = 0
+    for rows in csvreader:
+        ProfitLoss = ProfitLoss + int(rows[1])
+    print(ProfitLoss)
+    
+    #Return to Second row in .csv
+    csvfile.seek(1)
+
+    #Start loop to get row count for number of months
+    row_count = sum(1 for row in csvreader)
+    print(f"Number of Months: {row_count}")
+csvfile.close()
