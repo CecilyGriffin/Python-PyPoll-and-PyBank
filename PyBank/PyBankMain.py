d15211c3ed7c092478f78dca8619f3c0f98c19a6
Mercy Griffin <teachmercy@gmail.com>
2020-05-08 21:37:55 -0500

    added title and average change

--- PyBank/PyBankMain.py
+++ PyBank/PyBankMain.py
@@ -5,24 +5,27 @@ import csv
 #Set path for file
 csvpath = os.path.join(".", "PyBankResources", "budget_data.csv")
 
+print("Financial Analysis")
+print("~~~~~~~~~~~~~~~~~~")
 #Read using CSV module
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     ProfitLoss = 0
     for rows in csvreader:
         ProfitLoss = ProfitLoss + int(rows[1])
-    print(ProfitLoss)
+    print(f"Profit: ${ProfitLoss}")
     
     #Return to Second row in .csv
     csvfile.seek(1)
 
-    #
-
     #Start loop to get row count for number of months
     row_count = sum(1 for row in csvreader)
     print(f"Number of Months: {row_count}")
+
+     #Average of the changes in profit losses
+    average = int(ProfitLoss/row_count)
+    print(f"Average Change ${average}")
 
 csvfile.close()
